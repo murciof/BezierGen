f2677dc6e38ce62c3a586a0b9c592a75070b8ff4
Murcio Filho <murciofilho94@gmail.com>
2020-04-09 20:46:47 -0300

    Document class and Database

---
+++ BezierGen/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezierGen
+{
+    class Database
+    {
+        Document document_a4 = new Document(210, 297, 0, 0, "A4 Paper (210mm x 297mm)");
+        Document document_bcard = new Document(80, 50, 0, 0, "Business Card (80mm x 50mm)");
+    }
+}
---
+++ BezierGen/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezierGen
+{
+    class Document
+    {
+        int dimensionX;
+        int dimensionY;
+        int marginX;
+        int marginY;
+        string documentType;
+        public Document(int dimensionX, int dimensionY, int marginX, int marginY, string documentType)
+        {
+            this.dimensionX = dimensionX;
+            this.dimensionY = dimensionY;
+            this.marginX = marginX;
+            this.marginY = marginY;
+            this.documentType = documentType;
+        }
+    }
+}
